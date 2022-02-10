135712144162aa8464e66711bffb5588d4e20c5e
imariom <marioallfred@gmail.com>
2022-02-10 13:22:41 -0800

    Now can create a new product

--- data/products.go
+++ data/products.go
@@ -3,41 +3,67 @@ package data
 import (
 	"encoding/json"
 	"io"
+	"sync"
 )
+
+// to protect read and write operations on the productList data store
+var rwMtx = &sync.RWMutex{}
+
+// in-memory product list data store
+var productList = Products{
+	&Product{
+		ID:          0,
+		Name:        "The Go Programming Language",
+		Description: "Modern, fast, reliable and productive programming language",
+		Category:    "books",
+		Image:       "",
+		Price:       49.99,
+	},
+}
 
 type Product struct {
 	ID          uint64  `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Category    string  `json:"category"`
-	SKU         string  `json:"sku"`
 	Image       string  `json:"image"`
 	Price       float64 `json:"price"`
 }
 
 type Products []*Product
 
-var productList = Products{
-	&Product{
-		ID:          0,
-		Name:        "Test product",
-		Description: "Product description",
-		Category:    "",
-		SKU:         "",
-		Image:       "",
-		Price:       0.0,
-	},
-}
-
 func GetAllProducts() Products {
+	rwMtx.RLock()
+	defer rwMtx.RUnlock()
+
 	return productList
 }
 
+func AddNewProduct(p *Product) {
+	p.ID = getNextID()
+
+	rwMtx.Lock()
+	productList = append(productList, p)
+	rwMtx.Unlock()
+}
+
 func getNextID() uint64 {
+	rwMtx.RLock()
+	// assumes the product with ID 0 will never be deleted
 	lastProduct := productList[len(productList)-1]
+	rwMtx.RUnlock()
+
 	return lastProduct.ID + 1
 }
 
-func (ps *Products) FromJSON(w io.Writer) error {
+func (ps *Products) ToJSON(w io.Writer) error {
 	return json.NewEncoder(w).Encode(ps)
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	return json.NewDecoder(r).Decode(p)
+}
+
+func (p *Product) ToJSON(w io.Writer) error {
+	return json.NewEncoder(w).Encode(p)
 }
--- handlers/product.go
+++ handlers/product.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"regexp"
@@ -10,6 +11,7 @@ import (
 
 var (
 	listProductsRe = regexp.MustCompile(`^/products[/]?$`)
+	createProductRe = regexp.MustCompile(`^/products[/]?$`)
 )
 
 type Product struct {
@@ -29,6 +31,10 @@ func (h *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		h.List(rw, r)
 		return
 
+	case r.Method == http.MethodPost && createProductRe.MatchString(r.URL.Path):
+		h.Create(rw, r)
+		return
+
 	default:
 		msg := "HTTP verb not implemented"
 		h.logger.Println(msg)
@@ -41,10 +47,38 @@ func (h *Product) List(rw http.ResponseWriter, r *http.Request) {
 	h.logger.Println("received a GET-list request")
 
 	products := data.GetAllProducts()
-	if err := products.FromJSON(rw); err != nil {
+	if err := products.ToJSON(rw); err != nil {
 		msg := "internal server error, while converting products to JSON"
 		h.logger.Println("[ERROR]", msg)
 		http.Error(rw, msg, http.StatusInternalServerError)
 		return
 	}
 }
+
+func (h *Product) Create(rw http.ResponseWriter, r *http.Request) {
+	h.logger.Println("received a POST request")
+
+	// create and store new product on the data store
+	newProduct := &data.Product{}
+	if err := newProduct.FromJSON(r.Body); err != nil {
+		http.Error(rw, "payload not valid", http.StatusBadRequest)
+		return
+	}
+	data.AddNewProduct(newProduct)
+
+	// try to return created product
+	if err := newProduct.ToJSON(rw); err != nil {
+		http.Error(rw,
+			fmt.Sprintf("product created with ID: '%d', but failed to retrieve it",
+				newProduct.ID),
+			http.StatusInternalServerError)
+	}
+}
+
+// Update product (PUT and PATCH)
+// Get all categories
+// Get products in a specific category
+// Limit results
+// Sort Results
+// Get a Single Product
+// Delete product
